f2c7941925521b6918bcc71f1148b84f15b3ac03
y579 <84591776+y579@users.noreply.github.com>
2021-09-24 19:27:36 -0700

    Create stringpath.hx

---
+++ source/stringpath.hx
@@ -0,0 +1,16 @@
+
+package;
+class Stringpath
+{
+  	inline static public function stringpath (key:String)
+	{
+		return 'assets/$key.png';
+	
+}
+	
+	
+	if (stringpath == assets\$key.png)
+	{
+		stringpath == assets/$key.png
+	};
+}
